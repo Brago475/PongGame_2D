53106e2a4d2af274b4d0187112ed44eb520aaf80
James Mardi <95704031+Brago475@users.noreply.github.com>
2025-02-20 11:58:22 -0500

    Update Paddle.java
    Code Update

--- PongGame_2D/Paddle.java
+++ PongGame_2D/Paddle.java
@@ -3,40 +3,36 @@ import java.awt.*;
 
 public class Paddle {
 
-    // Instance variables
+   
     private int height, x, y, initialY, speed;
     private Color color;
 
-    // Width of paddle will always be 15 for this app
+   
     static final int PADDLE_WIDTH = 15;
 
-    // Constructor
     public Paddle(int x, int y, int height, int speed, Color color) {
         this.x = x;
         this.y = y;
         this.height = height;
         this.speed = speed;
         this.color = color;
-        this.initialY = y; // Store the initial y position for resetting
+        this.initialY = y; 
     }
 
-    // Paint the paddle
+   
     public void paint(Graphics g) {
         g.setColor(color);
         g.fillRect(x, y, PADDLE_WIDTH, height);
     }
 
-    // Move paddle up
     public void moveUp() {
         y -= speed * 2;
     }
 
-    // Move paddle down
     public void moveDown() {
         y += speed * 2;
     }
 
-    // Check if this paddle collides with a ball
     public boolean checkCollision(Ball b) {
         int rightX = x + PADDLE_WIDTH;
         int bottomY = y + height;
@@ -49,17 +45,14 @@ public class Paddle {
         return false;
     }
 
-    // Set a new speed for the paddle
     public void setSpeed(int newSpeed) {
         this.speed = newSpeed;
     }
 
-    // Reset paddle to its original y position
     public void resetPosition() {
         this.y = initialY;
     }
 
-    // Accessors
     public int getX() {
         return x;
     }
